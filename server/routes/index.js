71f100def10dfc358d5acd367565372f29928a42
Harris Wang <harris.wang@mail.com>
2019-11-22 01:38:06 +1100

    init proj

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+const payslipController = require('../controllers/payslip');
+
+// router.post('/payslip/create', payslipController.create);
+// router.get('/payslip', payslipController.getAll);
+
+module.exports = router;
